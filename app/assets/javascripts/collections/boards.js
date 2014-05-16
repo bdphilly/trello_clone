523a5370eccf68b869386a84d7213ea39c9264e7
Bryan Phillips <bdphilly@gmail.com>
2014-05-16 10:51:26 -0700

    initial commit

---
+++ app/assets/javascripts/collections/boards.js
@@ -0,0 +1,11 @@
+Trellino.Collections.Boards = Backbone.Collection.extend ({
+	model: Trellino.Models.Board,
+	
+	url: "/api/boards",
+
+	
+
+
+
+
+});
---
+++ app/assets/javascripts/routers/router.js
@@ -0,0 +1,11 @@
+Trellino.Routers.router = Backbone.Router.extend ({
+	routes: {
+		"": "index",
+	},
+
+	// index: {
+	// 	Trellino.Collections.	
+	// }
+
+
+});
